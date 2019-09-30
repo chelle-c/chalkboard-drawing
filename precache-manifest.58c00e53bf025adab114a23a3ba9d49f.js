418e5554988fb033c495cbacd0bfd8b3b66498e7
Chelle <9515162+chelle-c@users.noreply.github.com>
2019-09-30 13:59:24 -0230

    Updates

---
+++ precache-manifest.58c00e53bf025adab114a23a3ba9d49f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9a16c2b5ffc22747f8c11f29a608fb",
+    "url": "/chalkboard-drawing/index.html"
+  },
+  {
+    "revision": "16b12c6a426cebb570ec",
+    "url": "/chalkboard-drawing/static/css/main.b61b8801.chunk.css"
+  },
+  {
+    "revision": "cacadff3eb2156543264",
+    "url": "/chalkboard-drawing/static/js/2.ad43cf9a.chunk.js"
+  },
+  {
+    "revision": "16b12c6a426cebb570ec",
+    "url": "/chalkboard-drawing/static/js/main.8105f848.chunk.js"
+  },
+  {
+    "revision": "1abbd860b19db7b7ffa1",
+    "url": "/chalkboard-drawing/static/js/runtime~main.24d010c0.js"
+  },
+  {
+    "revision": "0c77842f90396480ba051c3cdcc4262f",
+    "url": "/chalkboard-drawing/static/media/eraser-variant.0c77842f.svg"
+  }
+]);
